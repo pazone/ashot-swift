98dace08c8d810897922540720c417a6f6145955
ivan <>
2018-06-26 00:19:53 +0200

    added project structure

---
+++ AshotTestApp/AshotSwift/AshotSwift/AshotSwift.h
@@ -0,0 +1,19 @@
+//
+//  AshotSwift.h
+//  AshotSwift
+//
+//  Created by ivan on 26.06.18.
+//  Copyright © 2018 AshotTools. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AshotSwift.
+FOUNDATION_EXPORT double AshotSwiftVersionNumber;
+
+//! Project version string for AshotSwift.
+FOUNDATION_EXPORT const unsigned char AshotSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AshotSwift/PublicHeader.h>
+
+
---
+++ AshotTestApp/Pods/Target Support Files/Pods-AshotTestAppTests/Pods-AshotTestAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AshotTestAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_AshotTestAppTests
+@end
