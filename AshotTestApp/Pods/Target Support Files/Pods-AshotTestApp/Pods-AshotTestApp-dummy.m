736fc97ce0281b885af0dd02d9cc5251943f9865
ivan <>
2018-06-26 00:26:24 +0200

    updated gitignore

--- AshotTestApp/Pods/Target Support Files/Pods-AshotTestApp/Pods-AshotTestApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AshotTestApp : NSObject
-@end
-@implementation PodsDummy_Pods_AshotTestApp
-@end
--- AshotTestApp/Pods/Target Support Files/Pods-AshotTestAppTests/Pods-AshotTestAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AshotTestAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_AshotTestAppTests
-@end
